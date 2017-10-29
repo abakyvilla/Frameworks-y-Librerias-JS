88f59f39b770198d8762bea093fa428cc0533679
Abaky Viso <abaquihatake@hotmail.com>
2017-10-29 02:15:07 -0600

    Construcción del evento al presionar cada elemento(dulce).

--- js/app.js
+++ js/app.js
@@ -18,9 +18,54 @@ var estado_titulo = true;
     //Ejecutar la Funcion cada cierto tiempo
     setInterval(cambiar_estado, 2000);
 
+    $(".elemento").on('click', function () {
+      //CREACION DE ARREGLOS POR COLUMNAS
+      var col1 = $(".col-1").children('img');
+      var col2 = $(".col-2").children('img');
+      var col3 = $(".col-3").children('img');
+      var col4 = $(".col-4").children('img');
+      var col5 = $(".col-5").children('img');
+      var col6 = $(".col-6").children('img');
+      var col7 = $(".col-7").children('img');
 
-  });
+      //CREACION DE UNA MATRIZ CON TODOS LOS ELEMENTOS DEL TABLERO
+      var matriz = [col1, col2, col3, col4,
+                    col5, col6, col7];
 
+      //OBTENCION DEL ELEMENTO PADRE PARA DETERMINAR EL NUMERO DE COLUMNA
+      var padre = $(this).parent().attr('class');
+      var str = padre.substring(4,5); // <-- extrar el numero de la clase
+      str = parseInt(str); // <--- convertir el string a INT
+      fila = str-1; // <--- numero de la columna
+
+      //OBTENCION DEL INDICE DEL ELEMENTO SELECCIONADO
+      var indice = $(this).index();
+
+      //OBTENCION DE LA RUTA DE LA IMAGEN DEL ELEMENTO SELECCIONADO
+      var ruta_imagen = $(this).attr('src');
+
+
+      console.log(indice, fila, ruta_imagen, matriz[fila][indice]);
+      
+
+
+    });
+
+});
+
+    /*
+
+    function comp_combinacion () {
+
+
+
+
+      //var matriz = [columna1, columna2, columna3, columna4, columna5, columna6, columna7];
+
+
+      //console.log(matriz);
+    }
+    */
 
     // FUNCION PARA LLENAR EL TABLERO
     function llenar_tablero () {
